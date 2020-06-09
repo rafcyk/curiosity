6372fafee2424116122a5126062634a9f7c664c0
Rafcyk <rafcy9@gmail.com>
2020-06-09 18:18:49 +0200

    Updates

---
+++ precache-manifest.438260b47467d8d7d01323c5fbff9812.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2c6e01d2fef134a34bf2ef97003d81",
+    "url": "/curiosity/index.html"
+  },
+  {
+    "revision": "931043e5b18cadedfbe8",
+    "url": "/curiosity/static/css/main.984b39b5.chunk.css"
+  },
+  {
+    "revision": "2f9c23d89f28ce89aaae",
+    "url": "/curiosity/static/js/2.ee0f332d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/curiosity/static/js/2.ee0f332d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931043e5b18cadedfbe8",
+    "url": "/curiosity/static/js/main.e7e6ece6.chunk.js"
+  },
+  {
+    "revision": "223a065aa3ac2f53ee6c",
+    "url": "/curiosity/static/js/runtime-main.3da787ea.js"
+  },
+  {
+    "revision": "f4543b33c83156dacd5672d6f370e475",
+    "url": "/curiosity/static/media/arrow.f4543b33.png"
+  },
+  {
+    "revision": "d201676f4e23f445281b8d4f7e1a676a",
+    "url": "/curiosity/static/media/close.d201676f.png"
+  },
+  {
+    "revision": "52fb8712d486f04388a995df3fdcff6b",
+    "url": "/curiosity/static/media/history1.52fb8712.png"
+  },
+  {
+    "revision": "83035d66382f7e5863daf4aa3bb1a911",
+    "url": "/curiosity/static/media/music1.83035d66.png"
+  },
+  {
+    "revision": "fcc50af49cb042a249590110709850ff",
+    "url": "/curiosity/static/media/smile.fcc50af4.png"
+  },
+  {
+    "revision": "bd16cb75775f3fd465b0e275d08f3c95",
+    "url": "/curiosity/static/media/sport1.bd16cb75.png"
+  }
+]);
